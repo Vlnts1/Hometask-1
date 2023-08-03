5868bb7c57db61561552eb3d82da8e0d1511b50f
Viktoria <Valentsionovaviktoria@gmail.com>
2023-08-04 02:10:31 +0300

    Add array of notes, fixed a layout

---
+++ js/data.js
@@ -0,0 +1,51 @@
+export const notes = [
+    {
+      name: "Dentist Appointment",
+      content: "I'm gonna have a dentist appointment on the 3/5/2021...",
+      category: "Task",
+      date: "2023-08-01",
+      archived: false
+    },
+    {
+      name: "Random Thought",
+      content: "What if the sky was actually green?",
+      category: "Random Thought",
+      date: "2023-08-02",
+      archived: false
+    },
+    {
+      name: "Brilliant Idea",
+      content: "I should start a business selling hats for cats!",
+      category: "Idea",
+      date: "2023-08-03",
+      archived: false
+    },
+    {
+      name: "Meeting with Team",
+      content: "Discuss project updates and plan for next sprint.",
+      category: "Task",
+      date: "2023-08-04",
+      archived: false
+    },
+    {
+      name: "Dream Vacation",
+      content: "Imagine relaxing on a beach in Bali...",
+      category: "Random Thought",
+      date: "2023-08-05",
+      archived: false
+    },
+    {
+      name: "App Development",
+      content: "Build a notes app with JS and HTML.",
+      category: "Task",
+      date: "2023-08-06",
+      archived: false
+    },
+    {
+      name: "Innovative Idea",
+      content: "Create a device that translates dog barks to human language.",
+      category: "Idea",
+      date: "2023-08-07",
+      archived: false
+    }
+];
